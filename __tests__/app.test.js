c96bc64672363c51138643ead9da3add4660903f
Isaac <isaac.ewing3@gmail.com>
2021-07-30 22:57:22 -0600

    basic generator works, need to link it with the solver to make sure theres a solution

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,14 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import generate from '../lib/utils/generator.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+  it('tests sudoku generation', () => {
+    const res = generate();
+    expect([]).toEqual(res);
   });
 });
